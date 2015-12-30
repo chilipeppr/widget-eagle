edd10f7c1b698ab5279a1d46b9098a3c267cea36
raykholo <raykholo@gmail.com>
2015-12-30 19:33:18 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- runme.js
+++ runme.js
@@ -1022,6 +1022,7 @@ var pushToGithubSync = function() {
   stdout += "> git add *\n";
   stdout += '> git commit -m "Made some changes to ChiliPeppr widget using Cloud9"\n';
   stdout += "> git push\n";
+  stdout += "> git push origin master:gh-pages\n";
   stdout += proc.execSync('git add *; git commit -m "Made some changes to ChiliPeppr widget using Cloud9"; git push;', { encoding: 'utf8' });
   console.log("Pushed to github sync. Stdout:", stdout);
   
