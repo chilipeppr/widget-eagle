ed7b18ba3206a56df9a5a5ca9813dcfd54281b4b
John Lauer <jlauer@chilipeppr.com>
2015-12-30 21:14:36 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- runme.js
+++ runme.js
@@ -406,6 +406,270 @@ This widget/element publishes the following signals. These signals are owned by 
 chilipeppr.subscribe(signal, callback) method. 
 To better understand how ChiliPeppr's subscribe() method works see amplify.js's documentation at http://amplifyjs.com/api/pubsub/
 
+  <table id="com-chilipeppr-elem-pubsubviewer-pub" class="table table-bordered table-striped">
+      <thead>
+          <tr>
+              <th style="">Signal</th>
+              <th style="">Description</th>
+          </tr>
+      </thead>
+      <tbody>
+          
+      $row-publish-start    
+      <tr><td colspan="2">(No signals defined in this widget/element)</td></tr>
+      $row-publish-end    
+      
+      </tbody>
+  </table>
+
+## Subscribe
+
+This widget/element subscribes to the following signals. These signals are owned by this widget/element. Other objects inside the ChiliPeppr environment can publish to these signals via the chilipeppr.publish(signal, data) method. 
+To better understand how ChiliPeppr's publish() method works see amplify.js's documentation at http://amplifyjs.com/api/pubsub/
+
+  <table id="com-chilipeppr-elem-pubsubviewer-sub" class="table table-bordered table-striped">
+      <thead>
+          <tr>
+              <th style="">Signal</th>
+              <th style="">Description</th>
+          </tr>
+      </thead>
+      <tbody>
+          
+      $row-subscribe-start    
+      <tr><td colspan="2">(No signals defined in this widget/element)</td></tr>
+      $row-subscribe-end    
+      
+      </tbody>
+  </table>
+
+## Foreign Publish
+
+This widget/element publishes to the following signals that are owned by other objects. 
+To better understand how ChiliPeppr's subscribe() method works see amplify.js's documentation at http://amplifyjs.com/api/pubsub/
+
+  <table id="com-chilipeppr-elem-pubsubviewer-foreignpub" class="table table-bordered table-striped">
+      <thead>
+          <tr>
+              <th style="">Signal</th>
+              <th style="">Description</th>
+          </tr>
+      </thead>
+      <tbody>
+          
+      $row-foreign-publish-start    
+      <tr><td colspan="2">(No signals defined in this widget/element)</td></tr>
+      $row-foreign-publish-end    
+      
+      </tbody>
+  </table>
+
+## Foreign Subscribe
+
+This widget/element publishes to the following signals that are owned by other objects.
+To better understand how ChiliPeppr's publish() method works see amplify.js's documentation at http://amplifyjs.com/api/pubsub/
+
+  <table id="com-chilipeppr-elem-pubsubviewer-foreignsub" class="table table-bordered table-striped">
+      <thead>
+          <tr>
+              <th style="">Signal</th>
+              <th style="">Description</th>
+          </tr>
+      </thead>
+      <tbody>
+      
+      $row-foreign-subscribe-start    
+      <tr><td colspan="2">(No signals defined in this widget/element)</td></tr>
+      $row-foreign-subscribe-end    
+      
+      </tbody>
+  </table>
+
+## Methods / Properties
+
+The table below shows, in order, the methods and properties inside the widget/element.
+
+  <table id="com-chilipeppr-elem-methodsprops" class="table table-bordered table-striped">
+      <thead>
+          <tr>
+              <th style="">Method / Property</th>
+              <th>Type</th>
+              <th style="">Description</th>
+          </tr>
+      </thead>
+      <tbody>
+          
+      $row-methods-start
+      <tr><td colspan="2">(No methods or properties defined in this widget/element)</td></tr>
+      $row-methods-end
+      
+      </tbody>
+  </table>
+
+
+## About ChiliPeppr
+
+[ChiliPeppr](http://chilipeppr.com) is a hardware fiddle, meaning it is a 
+website that lets you easily
+create a workspace to fiddle with your hardware from software. ChiliPeppr provides
+a [Serial Port JSON Server](https://github.com/johnlauer/serial-port-json-server) 
+that you run locally on your computer, or remotely on another computer, to connect to 
+the serial port of your hardware like an Arduino or other microcontroller.
+
+You then create a workspace at ChiliPeppr.com that connects to your hardware 
+by starting from scratch or forking somebody else's
+workspace that is close to what you are after. Then you write widgets in
+Javascript that interact with your hardware by forking the base template 
+widget or forking another widget that
+is similar to what you are trying to build.
+
+ChiliPeppr is massively capable such that the workspaces for 
+[TinyG](http://chilipeppr.com/tinyg) and [Grbl](http://chilipeppr.com/grbl) CNC 
+controllers have become full-fledged CNC machine management software used by
+tens of thousands.
+
+ChiliPeppr has inspired many people in the hardware/software world to use the
+browser and Javascript as the foundation for interacting with hardware. The
+Arduino team in Italy caught wind of ChiliPeppr and now
+ChiliPeppr's Serial Port JSON Server is the basis for the 
+[Arduino's new web IDE](https://create.arduino.cc/). If the Arduino team is excited about building on top
+of ChiliPeppr, what
+will you build on top of it?
+
+`
+
+  var widgetUrl = 'http://' +
+    process.env.C9_PROJECT + '-' + process.env.C9_USER +
+    '.c9users.io/widget.html';
+  var testUrl = 'https://preview.c9users.io/' +
+    process.env.C9_USER + '/' +
+    process.env.C9_PROJECT + '/widget.html';
+  var editUrl = 'http://ide.c9.io/' +
+    process.env.C9_USER + '/' +
+    process.env.C9_PROJECT;
+  var github = getGithubUrl();
+
+  md = md.replace(/\$widget-id/g, widget.id);
+  md = md.replace(/\$widget-name/g, widget.name);
+  md = md.replace(/\$widget-desc/g, widget.desc);
+  md = md.replace(/\$widget-cpurl/g, github.rawurl);
+  md = md.replace(/\$widget-editurl/g, editUrl);
+  md = md.replace(/\$widget-giturl/g, github.url);
+  md = md.replace(/\$widget-testurl/g, testUrl);
+  
+  var cpload = generateCpLoadStmt();
+  md = md.replace(/\$widget-cploadjs/g, cpload);
+
+  // see if there is a screenshot, if so use it
+  var img = "";
+  if (fs.existsSync("screenshot.png")) {
+    img = "![alt text]" + 
+    "(screenshot.png \"Screenshot\")";
+  }
+  md = md.replace(/\$widget-img/g, img);
+
+  /*
+  // now generate methods/properties
+  //$widget-methprops
+  var s = "";
+  for (var key in widget) {
+    var obj = widget[key];
+    s += '| ' + key +
+      ' | ' + typeof obj +
+      ' | ';
+    s += widgetDocs[key].descHtml.replace(/[\r\n]/g, "");
+    s += ' |\n';
+  }
+  //console.log("adding markdown:", s);
+  md = md.replace(/\$widget-methprops/g, s);
+
+
+  // now do pubsub signals
+  var s;
+  s = appendKeyValForMarkdown(widget.publish);
+  md = md.replace(/\$widget-publish/, s);
+  s = appendKeyValForMarkdown(widget.subscribe);
+  md = md.replace(/\$widget-subscribe/, s);
+  s = appendKeyValForMarkdown(widget.foreignPublish);
+  md = md.replace(/\$widget-foreignpublish/, s);
+  s = appendKeyValForMarkdown(widget.foreignSubscribe);
+  md = md.replace(/\$widget-foreignsubscribe/, s);
+  */
+  
+    // do the properties and methods
+  var s = "";
+  for (var key in widget) {
+    var obj = widget[key];
+    s += '<tr><td>' + key +
+      '</td><td>' + typeof obj +
+      '</td><td>';
+    s += widgetDocs[key].descHtml;
+    s += '</td></tr>';
+  }
+  md = md.replace(/\$row-methods-start[\s\S]+?\$row-methods-end/g, s);
+
+  // now do pubsub signals
+  var s;
+  s = appendKeyVal(widget.publish);
+  md = md.replace(/\$row-publish-start[\s\S]+?\$row-publish-end/, s);
+  s = appendKeyVal(widget.subscribe);
+  md = md.replace(/\$row-subscribe-start[\s\S]+?\$row-subscribe-end/g, s);
+  s = appendKeyVal(widget.foreignPublish);
+  md = md.replace(/\$row-foreign-publish-start[\s\S]+?\$row-foreign-publish-end/, s);
+  s = appendKeyVal(widget.foreignSubscribe);
+  md = md.replace(/\$row-foreign-subscribe-start[\s\S]+?\$row-foreign-subscribe-end/g, s);
+
+  // now write out the auto-gen file
+  fs.writeFileSync("README.md", md);
+  console.log("Rewrote README.md");
+  
+}
+
+var generateWidgetReadmeOld = function() {
+
+  // First we have to eval so stuff is in memory
+  evalWidgetJs();
+  
+  // Spit out Markdown docs
+  var md = `# $widget-id
+$widget-desc
+
+$widget-img
+
+## ChiliPeppr $widget-name
+
+All ChiliPeppr widgets/elements are defined using cpdefine() which is a method
+that mimics require.js. Each defined object must have a unique ID so it does
+not conflict with other ChiliPeppr widgets.
+
+| Item                  | Value           |
+| -------------         | ------------- | 
+| ID                    | $widget-id |
+| Name                  | $widget-name |
+| Description           | $widget-desc |
+| chilipeppr.load() URL | $widget-cpurl |
+| Edit URL              | $widget-editurl |
+| Github URL            | $widget-giturl |
+| Test URL              | $widget-testurl |
+
+## Example Code for chilipeppr.load() Statement
+
+You can use the code below as a starting point for instantiating this widget 
+inside a workspace or from another widget. The key is that you need to load 
+your widget inlined into a div so the DOM can parse your HTML, CSS, and 
+Javascript. Then you use cprequire() to find your widget's Javascript and get 
+back the instance of it.
+
+\`\`\`javascript
+$widget-cploadjs
+\`\`\`
+
+## Publish
+
+This widget/element publishes the following signals. These signals are owned by this widget/element and are published to all objects inside the ChiliPeppr environment that listen to them via the 
+chilipeppr.subscribe(signal, callback) method. 
+To better understand how ChiliPeppr's subscribe() method works see amplify.js's documentation at http://amplifyjs.com/api/pubsub/
+
 | Signal | Description |
 | ------ | ----------- |
 $widget-publish
